c85980f8dacffacad779fc5828242489a8d8352e
张杰 <165590438@qq.com>
2017-08-09 22:19:39 +0800

    11

---
+++ tool/spider/dirbot/dirbot/spiders/dmoz.py
@@ -0,0 +1,40 @@
+from scrapy.spiders import Spider
+from scrapy.selector import Selector
+
+from dirbot.items import Website
+import re
+import json
+
+
+from scrapy.selector import Selector
+try:
+    from scrapy.spider import Spider
+except:
+    from scrapy.spider import BaseSpider as Spider
+from scrapy.utils.response import get_base_url
+from scrapy.utils.url import urljoin_rfc
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor as sle
+
+class DmozSpider(CrawlSpider):
+    name = "dmoz"
+    allowed_domains = ["tencent.com"]
+    start_urls = [
+        "http://hr.tencent.com/position.php"
+    ]
+    rules = [
+        Rule(sle(allow=("/position.php\?&start=\d{,4}#a")), follow=True, callback='parse_item')
+    ]
+
+    def parse_item(self, response):
+        items = []
+        sel = Selector(response)
+        base_url = get_base_url(response)
+        sites_even = sel.css('table.tablelist tr.even')
+        for site in sites_even:
+            item = Website()
+            item['name'] = site.css('.l.square a').xpath('text()').extract()[0]
+            item['description'] = site.css('tr > td:nth-child(2)::text').extract()[0]
+            item['url'] = site.css('tr > td:nth-child(4)::text').extract()[0]
+            items.append(item)
+        return items
