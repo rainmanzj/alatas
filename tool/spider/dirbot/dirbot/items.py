ad152a3f35560e17116bb24b879be2591f7d155d
张杰 <165590438@qq.com>
2017-08-11 10:33:40 +0800

    2

--- tool/spider/dirbot/dirbot/items.py
+++ tool/spider/dirbot/dirbot/items.py
@@ -2,7 +2,10 @@ from scrapy.item import Item, Field
 
 
 class Website(Item):
-
+    uid = Field()
     name = Field()
     description = Field()
     url = Field()
+    address=Field()
+    num=Field()
+    
--- tool/spider/dirbot/dirbot/spiders/dmoz.py
+++ tool/spider/dirbot/dirbot/spiders/dmoz.py
@@ -38,9 +38,13 @@ class DmozSpider(CrawlSpider):
         sites_even = sel.css('table.tablelist tr.even')
         for site in sites_even:
             item = Website()
+            
             item['name'] = site.css('.l.square a').xpath('text()').extract()[0]
             item['description'] = site.css('tr > td:nth-child(2)::text').extract()[0]
             url=site.css('tr > td:nth-child(4)::text').extract()[0]
             item['url'] = p.get_initials(url,u'')
+            item['address'] = url
+            item['num'] = site.css('tr > td:nth-child(3)::text').extract()[0]
+            item['uid'] = site.css('.l.square a').xpath('text()').extract()[0]
             items.append(item)
         return items
